fd9a092463921c22faa90c55836de853a928ac19
DianaNatali <dnmaldonador@unal.edu.co>
2024-06-14 17:58:15 -0500

    Minor.

--- src/spi_control.sv
+++ src/spi_control.sv
@@ -14,9 +14,9 @@ module spi_control (
         input logic     spi_cs_i,
         output logic    spi_sdo_o,
 
-        //Sobel Interface
-        output logic    [MAX_PIXEL_BITS-1:0] input_px_gray_o,
-        input logic     [MAX_PIXEL_BITS-1:0] output_px_sobel_i,
+        //Gray/Sobel Interface
+        output logic    [MAX_PIXEL_BITS-1:0] input_px_o,
+        input logic     [MAX_PIXEL_BITS-1:0] output_px_i,
         input logic     px_rdy_o_spi_i,
         output logic    px_rdy_i_spi_o
     );
@@ -29,14 +29,84 @@ module spi_control (
     logic spi_rxtx_done;
     logic rxtx_done;
     logic rxtx_done_reg;
+    logic ncs_signal;
 
-    // nreset synchronization
     spi_dep_signal_synchronizer signal_sync1 (
         .clk_i(clk_i),
         .nreset_i(nreset_i),
         .async_signal_i(spi_rxtx_done),
         .signal_o(rxtx_done)
     );
+
+    spi_dep_signal_synchronizer signal_sync2 (
+        .clk_i(clk_i),
+        .nreset_i(nreset_i),
+        .async_signal_i(~spi_cs_i),
+        .signal_o(ncs_signal)
+    );
+
+    typedef enum logic [1:0] {
+        S_IDLE,
+        S_TXRX_INIT,
+        S_TXRX
+    } state_e;
+
+    state_e state, next_state;
+    logic txrx_active;
+
+    always_ff @(posedge clk_i or negedge nreset_i) begin
+        if (!nreset_i)
+            state <= S_IDLE;
+		else
+            state <= next_state;
+	end
+
+    always_comb begin
+        case(state)
+            S_IDLE: begin
+                if(ncs_signal)
+                    next_state = S_TXRX_INIT;
+                else
+                    next_state = S_IDLE;
+            end
+            S_TXRX_INIT: begin
+                if(rxtx_done)
+                    next_state = S_TXRX;
+                else
+                    next_state = S_TXRX_INIT;
+            end
+            S_TXRX: begin
+                if(~ncs_signal)
+                    next_state = S_IDLE;
+                else
+                    next_state = S_TXRX;
+            end
+            default:
+                next_state = S_IDLE;
+        endcase
+    end
+
+    always_ff @(posedge clk_i or negedge nreset_i) begin
+        if(!nreset_i) begin
+            txrx_active <= 1'b0;
+        end else begin
+            case(next_state)
+                S_IDLE: begin
+                    txrx_active <= 1'b0;
+                end
+                S_TXRX_INIT: begin
+                    txrx_active <= 1'b1;
+                end
+                S_TXRX: begin
+                    txrx_active <= 1'b0;
+                end
+                default : begin
+                    txrx_active <= 1'b0;
+                end
+            endcase
+        end
+    end
+
 
     logic rxtx_done_rising;
     assign rxtx_done_rising = rxtx_done & ~rxtx_done_reg;
@@ -63,7 +133,7 @@ module spi_control (
             data_tx <= '0;
         end else begin
             if(px_rdy_o_spi_i)begin
-                data_tx <= output_px_sobel_i;
+                data_tx <= output_px_i;
             end
         end
     end
@@ -81,6 +151,6 @@ module spi_control (
         .rxtx_done_o(spi_rxtx_done)
     );
 
-    assign input_px_gray_o = data_rx;
+    assign input_px_o = data_rx;
 
 endmodule
