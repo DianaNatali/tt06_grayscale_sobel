42377e659a09d5a63272835042f24b100b50eb2d
DianaNatali <dnmaldonador@unal.edu.co>
2024-06-14 18:00:16 -0500

    Adding all preprocessing configuration.

--- test/rpi_test/gray_sobel_rpi.py
+++ test/rpi_test/gray_sobel_rpi.py
@@ -2,6 +2,7 @@
 import gpiozero as gpio
 import spidev
 import numpy as np
+from emulation_gray import *
 
 class SpiBus:
     def __init__(self, freq=1000000):
@@ -9,7 +10,6 @@ class SpiBus:
         self.spi.open(0, 0)  # Open SPI bus 0, device 0
         self.spi.max_speed_hz = freq  # Set SPI speed
         self.spi.mode = 0b11  # SPI mode
-        self.setup_gpio()
 
     def spi_transfer(self, data):
         data &= 0xFFFFFF
@@ -20,10 +20,10 @@ class SpiBus:
     def set_freq(self, freq):
         self.spi.max_speed_hz = freq  # Set SPI speed
 
-class DianaChip:
+class ImgPreprocessingChip:
     def __init__(self, spi:SpiBus=None):
         self.spi_bus = spi
-        self.nreset = gpio.OutputDevice()
+        self.nreset = gpio.OutputDevice(18, active_high=False, initial_value=False )
         self.select_process_bit0 = gpio.OutputDevice(17, active_high=False, initial_value=False)
         self.select_process_bit1 = gpio.OutputDevice(27, active_high=False, initial_value=False)
         self.start_sobel = gpio.OutputDevice(22, active_high=False, initial_value=False)
@@ -36,22 +36,39 @@ class DianaChip:
         self.select_process_bit1.on()
         self.start_sobel.off()
 
+    def set_gray_conf(self):
+        self.select_process_bit0.off()
+        self.select_process_bit1.on()
+        self.start_sobel.off()
+
+    def set_sobel_conf(self):
+        self.select_process_bit0.off()
+        self.select_process_bit1.on()
+        self.start_sobel.on()
+
+    def set_graysobel_conf(self):
+        self.select_process_bit0.off()
+        self.select_process_bit1.off()
+        self.start_sobel.on()
+
     def echo(self):
         self.nreset.on()
 
         N = 10
-        random_numbers_array = np.ones(10)*0x2
+        random_numbers_array = np.random.randint(0, 2**24, N, dtype=np.uint32)
         
         for i, data in enumerate(random_numbers_array):
+            print(hex(data))
             received_data = self.spi_bus.spi_transfer(int(data))
+            print(f'{i} {int.from_bytes(received_data[3:], "little"):x} {emulation_gray(data):x}')
             hex_data = [hex(x) for x in received_data]
             print(hex_data)
-            print(f'{int.from_bytes(received_data[3:], "little"):x}')
+            
 
 
 if __name__ == "__main__":
     bus_spi = SpiBus()
-    chip = DianaChip(spi=bus_spi)
+    chip = ImgPreprocessingChip(spi=bus_spi)
     chip.set_bypass_conf()
     chip.echo()
         
