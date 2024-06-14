9944b633800f714ba6fc543dabb34ca781792a58
DianaNatali <dnmaldonador@unal.edu.co>
2024-06-14 14:55:17 -0500

    Refactoring.

--- test/rpi_test/sobel_test.py
+++ test/rpi_test/sobel_test.py
@@ -1,95 +1,57 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
+import gpiozero as gpio
 import spidev
 import numpy as np
-import time
 
-
-class SpiTestBase:
-    def __init__(self):
+class SpiBus:
+    def __init__(self, freq=1000000):
         self.spi = spidev.SpiDev()
         self.spi.open(0, 0)  # Open SPI bus 0, device 0
-        self.spi.max_speed_hz = 50000000  # Set SPI speed
-        self.spi.mode = 0b10  # SPI mode
+        self.spi.max_speed_hz = freq  # Set SPI speed
+        self.spi.mode = 0b11  # SPI mode
         self.setup_gpio()
-
-    def setup_gpio(self):
-        GPIO.setmode(GPIO.BCM)
-        
-        self.select_process_bit0 = 17
-        self.select_process_bit1 = 27
-        self.start_sobel = 22
-
-        self.LFSR_enable = 2
-        self.seed_stop =  3
-        self.lfsr_en_i =  4
-
-        input_pins = [self.select_process_bit0, self.select_process_bit1, self.start_sobel, self.LFSR_enable, self.seed_stop, self.lfsr_en_i]
-        
-        for pin in input_pins:
-            GPIO.setup(pin, GPIO.OUT)
-        
-        self.lfsr_done = 5
-        self.ena_o = 6
-
-        self.out_px_bit0 = 26
-        self.out_px_bit1 = 18
-        self.out_px_bit2 = 23
-        self.out_px_bit3 = 24
-        self.out_px_bit4 = 25
-
-        output_pins = [self.lfsr_done, self.ena_o, self.out_px_bit0, self.out_px_bit1, self.out_px_bit2, self.out_px_bit3, self.out_px_bit4]
-
-        for pin in output_pins:
-            GPIO.setup(pin, GPIO.IN)
 
     def spi_transfer(self, data):
         data &= 0xFFFFFF
-        response = self.spi.xfer2([(data >> 16) & 0xFF , (data >> 8) & 0xFF, data & 0xFF])
-        print(response)
-        return (response[0] << 16) | (response[1] << 8) | response[2]
+        msg = data.to_bytes(6, 'little')
+        response = self.spi.xfer2(msg)
+        return response
 
-    def cleanup(self):
-        self.spi.close()
-        GPIO.cleanup()
+    def set_freq(self, freq):
+        self.spi.max_speed_hz = freq  # Set SPI speed
 
-class SpiBypassTest(SpiTestBase):
-    def __init__(self):
-        super().__init__()
-    
-    def run_test(self):
-        self.setup_gpio()
-        
-        # Selection = 3
-        GPIO.output(self.select_process_bit0, GPIO.HIGH)
-        GPIO.output(self.select_process_bit1, GPIO.HIGH)
+class DianaChip:
+    def __init__(self, spi:SpiBus=None):
+        self.spi_bus = spi
+        self.nreset = gpio.OutputDevice()
+        self.select_process_bit0 = gpio.OutputDevice(17, active_high=False, initial_value=False)
+        self.select_process_bit1 = gpio.OutputDevice(27, active_high=False, initial_value=False)
+        self.start_sobel = gpio.OutputDevice(22, active_high=False, initial_value=False)
+        self.LFSR_enable = gpio.OutputDevice(2, active_high=False, initial_value=False)
+        self.seed_stop = gpio.OutputDevice(3, active_high=False, initial_value=False)
+        self.lfsr_en_i = gpio.OutputDevice(4, active_high=False, initial_value=False)
 
-        # NOT LFSR
-        GPIO.output(self.LFSR_enable, GPIO.LOW)
-        GPIO.output(self.seed_stop, GPIO.LOW)
-        GPIO.output(self.lfsr_en_i, GPIO.LOW)
+    def set_bypass_conf(self):
+        self.select_process_bit0.on()
+        self.select_process_bit1.on()
+        self.start_sobel.off()
 
-        #Not sobel
-        GPIO.output(self.start_sobel, GPIO.LOW)
-        
-        N = 4
-        random_numbers_array = [0x000000, 0xFFFFFF, 0x123456, 0xABCDEF]
+    def echo(self):
+        self.nreset.on()
 
-        for number in random_numbers_array:
-            print(f"{number} {number:06x}")
+        N = 10
+        random_numbers_array = np.ones(10)*0x2
         
         for i, data in enumerate(random_numbers_array):
-            received_data = self.spi_transfer(int(data))
-            print(f"Respuesta recibida (en bits): {received_data:06x}")
+            received_data = self.spi_bus.spi_transfer(int(data))
+            hex_data = [hex(x) for x in received_data]
+            print(hex_data)
+            print(f'{int.from_bytes(received_data[3:], "little"):x}')
 
-            # if i > 0:
-            #     #print(f"{i} {read_data:x} {random_numbers_array[i-1]:x}")
-            #     assert read_data == random_numbers_array[i-1]
 
 if __name__ == "__main__":
-    try:
-        bypass_test = SpiBypassTest()
-        bypass_test.run_test()
-
-    finally:
-        bypass_test.cleanup()
+    bus_spi = SpiBus()
+    chip = DianaChip(spi=bus_spi)
+    chip.set_bypass_conf()
+    chip.echo()
         
